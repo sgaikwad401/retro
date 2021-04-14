6cb0ef9eb4af4f4b79aa64430fd8853589287528
sgaikwad <sgaikwad@gmail.com>
2021-04-14 16:00:11 +0530

    First Commit

---
+++ src/test/java/com/retrocompany/first_retro_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.retrocompany.first_retro_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
